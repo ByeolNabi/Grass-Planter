e18107f8f6eb8f96575b6fe1f65dec45396428b2
ByeolNabi <20183338@office.deu.ac.kr>
2024-01-23 23:33:21 +0900

    [feat] 커밋 상태 블럭 출력하는 코드 추가
    - config에서 json을 가져옴
    - config의 데이터를 for문을 이용해서 순회함

---
+++ JS.js
@@ -0,0 +1,25 @@
+function headerGrass(userName){
+    var header = `<h1>${userName}'s state</h1>`;
+    return header;
+}
+
+function grassImgTag(userName){
+    var imgTag = `<img src="https://ghchart.rshah.org/${userName}" alt="${userName}'s chart"></img>`;
+    return imgTag;
+}
+
+function grassDisplay(userName){
+    var display = headerGrass(userName) + '<br>' + grassImgTag(userName);
+    return display
+}
+
+// 깡통 div를 만들고 그 안에 text를 넣어보자
+for(var i = 0; i < config.member.length; ++i){
+    var div = document.createElement("div");
+    div.setAttribute("class","grassDisplay");
+    div.innerHTML = grassDisplay(config.member[i].githubUserName);
+
+    document.body.appendChild(div)
+}
+
+//grassDisplay(config.member[i].githubUserName)
