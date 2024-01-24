5084ba7a62fe49731268c2dd4a30bc8ad51e7da3
ByeolNabi <20183338@office.deu.ac.kr>
2024-01-25 00:23:10 +0900

    #3 반복문을 함수로 묶음

--- JS.js
+++ JS.js
@@ -13,13 +13,15 @@ function grassDisplay(userName){
     return display
 }
 
-// 깡통 div를 만들고 그 안에 text를 넣어보자
-for(var i = 0; i < config.member.length; ++i){
-    var div = document.createElement("div");
-    div.setAttribute("class","grassDisplay");
-    div.innerHTML = grassDisplay(config.member[i].githubUserName);
+function grassBoard(config){
+    // 깡통 div를 만들고 그 안에 text를 넣어보자
+    for(var i = 0; i < config.member.length; ++i){
+        var div = document.createElement("div");
+        div.setAttribute("class","grassDisplay");
+        div.innerHTML = grassDisplay(config.member[i].githubUserName);
 
-    document.body.appendChild(div)
+        document.body.appendChild(div)
+    }
 }
 
-//grassDisplay(config.member[i].githubUserName)
+grassBoard(config)
